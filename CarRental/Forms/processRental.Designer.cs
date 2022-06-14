a6c1364183577d7dd5e3494f6932c847c2c732a6
Areekadan <59070022+Areekadan@users.noreply.github.com>
2022-06-13 23:48:21 -0600

    employee screens

---
+++ CarRental/Forms/processRental.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace CarRental.Forms
+{
+    partial class processRental
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.carIDRental = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // carIDRental
+            // 
+            this.carIDRental.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.carIDRental.AutoSize = true;
+            this.carIDRental.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.carIDRental.ForeColor = System.Drawing.Color.LimeGreen;
+            this.carIDRental.Location = new System.Drawing.Point(61, 142);
+            this.carIDRental.Name = "carIDRental";
+            this.carIDRental.Size = new System.Drawing.Size(61, 21);
+            this.carIDRental.TabIndex = 0;
+            this.carIDRental.Text = "CAR ID";
+            this.carIDRental.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox1.Location = new System.Drawing.Point(23, 166);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(172)))), ((int)(((byte)(19)))));
+            this.button1.FlatAppearance.BorderSize = 6;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(334, 744);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 40);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "AUTHORIZE";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(172)))), ((int)(((byte)(19)))));
+            this.textBox3.Location = new System.Drawing.Point(440, 28);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PlaceholderText = "ENTER CUSTOMER ID";
+            this.textBox3.Size = new System.Drawing.Size(600, 23);
+            this.textBox3.TabIndex = 24;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(195, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(1073, 658);
+            this.dataGridView1.TabIndex = 25;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox2.Location = new System.Drawing.Point(22, 254);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 23);
+            this.textBox2.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label1.Location = new System.Drawing.Point(40, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 21);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "PICKUP DATE";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox4.Location = new System.Drawing.Point(22, 347);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(140, 23);
+            this.textBox4.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label2.Location = new System.Drawing.Point(34, 323);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 21);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "RETURN DATE";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label3.Location = new System.Drawing.Point(30, 420);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 21);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "PICKUP BRANCH";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label4.Location = new System.Drawing.Point(23, 517);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 21);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "RETURN BRANCH";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox7.Location = new System.Drawing.Point(23, 635);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(140, 23);
+            this.textBox7.TabIndex = 35;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label5.Location = new System.Drawing.Point(61, 611);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 21);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "TYPE ID";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(23, 444);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 23);
+            this.comboBox1.TabIndex = 36;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(23, 541);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(140, 23);
+            this.comboBox2.TabIndex = 37;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button2.BackColor = System.Drawing.Color.Chartreuse;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(172)))), ((int)(((byte)(19)))));
+            this.button2.FlatAppearance.BorderSize = 6;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button2.Location = new System.Drawing.Point(831, 744);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(300, 40);
+            this.button2.TabIndex = 38;
+            this.button2.Text = "UPGRADE";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // processRental
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(1306, 821);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.carIDRental);
+            this.Name = "processRental";
+            this.Text = "processRental";
+            this.Load += new System.EventHandler(this.processRental_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label carIDRental;
+        private TextBox textBox1;
+        private Button button1;
+        private TextBox textBox3;
+        private DataGridView dataGridView1;
+        private TextBox textBox2;
+        private Label label1;
+        private TextBox textBox4;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox7;
+        private Label label5;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Button button2;
+    }
+}
---
+++ CarRental/employeeMenuSkeleton.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace CarRental
+{
+    partial class employeeMenuSkeleton
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sideBar = new System.Windows.Forms.Panel();
+            this.reports = new System.Windows.Forms.Button();
+            this.returns = new System.Windows.Forms.Button();
+            this.rentals = new System.Windows.Forms.Button();
+            this.inventory = new System.Windows.Forms.Button();
+            this.availability = new System.Windows.Forms.Button();
+            this.logoBox = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.topBar = new System.Windows.Forms.Panel();
+            this.empName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.formsPane = new System.Windows.Forms.Panel();
+            this.sideBar.SuspendLayout();
+            this.logoBox.SuspendLayout();
+            this.topBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sideBar
+            // 
+            this.sideBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(43)))), ((int)(((byte)(194)))));
+            this.sideBar.Controls.Add(this.reports);
+            this.sideBar.Controls.Add(this.returns);
+            this.sideBar.Controls.Add(this.rentals);
+            this.sideBar.Controls.Add(this.inventory);
+            this.sideBar.Controls.Add(this.availability);
+            this.sideBar.Controls.Add(this.logoBox);
+            this.sideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sideBar.Location = new System.Drawing.Point(0, 0);
+            this.sideBar.Name = "sideBar";
+            this.sideBar.Size = new System.Drawing.Size(230, 976);
+            this.sideBar.TabIndex = 0;
+            this.sideBar.Paint += new System.Windows.Forms.PaintEventHandler(this.sideBar_Paint);
+            // 
+            // reports
+            // 
+            this.reports.Dock = System.Windows.Forms.DockStyle.Top;
+            this.reports.FlatAppearance.BorderSize = 0;
+            this.reports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.reports.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.reports.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.reports.Location = new System.Drawing.Point(0, 500);
+            this.reports.Name = "reports";
+            this.reports.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.reports.Size = new System.Drawing.Size(230, 95);
+            this.reports.TabIndex = 5;
+            this.reports.Text = "Reports";
+            this.reports.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.reports.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.reports.UseVisualStyleBackColor = true;
+            this.reports.Click += new System.EventHandler(this.reports_Click);
+            // 
+            // returns
+            // 
+            this.returns.Dock = System.Windows.Forms.DockStyle.Top;
+            this.returns.FlatAppearance.BorderSize = 0;
+            this.returns.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.returns.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.returns.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.returns.Location = new System.Drawing.Point(0, 405);
+            this.returns.Name = "returns";
+            this.returns.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.returns.Size = new System.Drawing.Size(230, 95);
+            this.returns.TabIndex = 4;
+            this.returns.Text = "Process Returns";
+            this.returns.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.returns.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.returns.UseVisualStyleBackColor = true;
+            this.returns.Click += new System.EventHandler(this.returns_Click);
+            // 
+            // rentals
+            // 
+            this.rentals.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rentals.FlatAppearance.BorderSize = 0;
+            this.rentals.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rentals.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rentals.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.rentals.Location = new System.Drawing.Point(0, 310);
+            this.rentals.Name = "rentals";
+            this.rentals.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.rentals.Size = new System.Drawing.Size(230, 95);
+            this.rentals.TabIndex = 3;
+            this.rentals.Text = "Process Rentals";
+            this.rentals.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.rentals.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.rentals.UseVisualStyleBackColor = true;
+            this.rentals.Click += new System.EventHandler(this.rentals_Click);
+            // 
+            // inventory
+            // 
+            this.inventory.Dock = System.Windows.Forms.DockStyle.Top;
+            this.inventory.FlatAppearance.BorderSize = 0;
+            this.inventory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.inventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.inventory.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.inventory.Location = new System.Drawing.Point(0, 215);
+            this.inventory.Name = "inventory";
+            this.inventory.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.inventory.Size = new System.Drawing.Size(230, 95);
+            this.inventory.TabIndex = 2;
+            this.inventory.Text = "Inventory Management";
+            this.inventory.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.inventory.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.inventory.UseVisualStyleBackColor = true;
+            this.inventory.Click += new System.EventHandler(this.inventory_Click);
+            // 
+            // availability
+            // 
+            this.availability.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(43)))), ((int)(((byte)(194)))));
+            this.availability.Dock = System.Windows.Forms.DockStyle.Top;
+            this.availability.FlatAppearance.BorderSize = 0;
+            this.availability.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.availability.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.availability.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.availability.Location = new System.Drawing.Point(0, 120);
+            this.availability.Name = "availability";
+            this.availability.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.availability.Size = new System.Drawing.Size(230, 95);
+            this.availability.TabIndex = 1;
+            this.availability.Text = "Availability and Cost";
+            this.availability.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.availability.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.availability.UseVisualStyleBackColor = false;
+            this.availability.Click += new System.EventHandler(this.availability_Click);
+            // 
+            // logoBox
+            // 
+            this.logoBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(52)))), ((int)(((byte)(166)))));
+            this.logoBox.Controls.Add(this.button1);
+            this.logoBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.logoBox.Location = new System.Drawing.Point(0, 0);
+            this.logoBox.Name = "logoBox";
+            this.logoBox.Size = new System.Drawing.Size(230, 120);
+            this.logoBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::CarRental.Properties.Resources.carLogo;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 97);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // topBar
+            // 
+            this.topBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(43)))), ((int)(((byte)(194)))));
+            this.topBar.Controls.Add(this.empName);
+            this.topBar.Controls.Add(this.label1);
+            this.topBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topBar.Location = new System.Drawing.Point(230, 0);
+            this.topBar.Name = "topBar";
+            this.topBar.Size = new System.Drawing.Size(1702, 120);
+            this.topBar.TabIndex = 1;
+            // 
+            // empName
+            // 
+            this.empName.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.empName.AutoSize = true;
+            this.empName.Font = new System.Drawing.Font("Vivaldi", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.empName.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.empName.Location = new System.Drawing.Point(1537, 80);
+            this.empName.Name = "empName";
+            this.empName.Size = new System.Drawing.Size(140, 22);
+            this.empName.TabIndex = 0;
+            this.empName.Text = "Employee Name";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(689, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(464, 77);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Branch Name";
+            // 
+            // formsPane
+            // 
+            this.formsPane.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formsPane.Location = new System.Drawing.Point(230, 120);
+            this.formsPane.Name = "formsPane";
+            this.formsPane.Size = new System.Drawing.Size(1702, 856);
+            this.formsPane.TabIndex = 2;
+            // 
+            // employeeMenuSkeleton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1932, 976);
+            this.Controls.Add(this.formsPane);
+            this.Controls.Add(this.topBar);
+            this.Controls.Add(this.sideBar);
+            this.Name = "employeeMenuSkeleton";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.logo_Load);
+            this.sideBar.ResumeLayout(false);
+            this.logoBox.ResumeLayout(false);
+            this.topBar.ResumeLayout(false);
+            this.topBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel sideBar;
+        private Panel logoBox;
+        private Button reports;
+        private Button returns;
+        private Button rentals;
+        private Button inventory;
+        private Button availability;
+        private Panel topBar;
+        private Label empName;
+        private Label label1;
+        private Button button1;
+        private Panel formsPane;
+    }
+}
